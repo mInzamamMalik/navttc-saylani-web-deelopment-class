66d65bbb9d887022d4f1ca1d6ff295ad10aac50d
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2020-12-22 18:03:15 +0500

    22 dec, 2020, today code

---
+++ server-side/class4.1_express second step/server.js
@@ -0,0 +1,49 @@
+
+
+let users = [];
+
+
+let express = require("express");
+var cors = require('cors')
+var morgan = require('morgan')
+var bodyParser = require('body-parser')
+
+let app = express();
+
+app.use(cors());
+
+// app.use(function (req, res, next) {
+//     console.log("Method is: ", req.method)
+//     console.log("URL is: ", req.url)
+//     console.log("connection is: ", req.connection.remoteAddress)
+//     console.log("connection is: ", req.connection.remotePort)
+//     // res.send("some response");
+//     next();
+// });
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.get("/bulb", function (req, res, next) {
+    res.send("bulb is On");
+});
+
+app.post("/bulb", function (req, res, next) {
+
+    console.log("body: ", req.body);
+    res.send("bulb is created");
+});
+
+app.put("/bulb", function (req, res, next) {
+    res.send("Bulb state is changed");
+});
+
+app.delete("/bulb", function (req, res, next) {
+    res.send("bulb is deleted");
+});
+
+
+
+app.listen(3000, () => {
+    console.log("server is running on 3000");
+})
---
+++ server-side/class4_hello world server without express/server.js
@@ -0,0 +1,12 @@
+var http = require("http");
+
+function requestHandler(request, response) {
+    console.log("In comes a request to: " + request.url);
+    console.log("In comes a request to: " + request.method);
+    console.log("In comes a request to: " + request.body);
+    response.end("Hello, world!");
+}
+
+var server = http.createServer(requestHandler);
+
+server.listen(3000); 
