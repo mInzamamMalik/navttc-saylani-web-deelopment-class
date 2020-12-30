ff8228185c9382367d65d2833ba9d5deac57d991
M Inzamam Malik <malikasinger1@gmail.com>
2020-12-30 17:44:53 +0500

    socket class code

--- server-side/class7.1_socket.io/server.js
+++ server-side/class7.1_socket.io/server.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var http = require("http");
-var socket = require('socket.io');
+var socketIO = require('socket.io');
 
 
 var app = express();
@@ -14,27 +14,43 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json())
 
-app.use("/", express.static(path.join(__dirname, 'public')))
+
+app.get("/signup", (req, res, next) => {
+    res.send("signup response");
+})
 
 
-
+app.use("/", express.static(path.resolve(path.join(__dirname, 'public'))))
 
 var server = http.createServer(app);
-var io = socket(server);
+
+var io = socketIO(server);
+
+var users = [];
+
+io.on('connection', (user) => {
+
+    console.log("user id: ", user.id);
+
+    user.emit("NOTIFICATION", "some data")
+
+    users.push(user);
+    console.log("user count: ", users.length);
 
 
+    setTimeout(function () {
+        users[0].emit("NOTIFICATION", "new message")
+    }, 10000)
 
-io.on('connection', (userAdress) => {
     console.log('a user connected');
 });
 
-io.on('disconnect', (socket) => {
-    console.log('a user connected');
-});
 
 
 
-
+setInterval(function () {
+    io.emit("COMMON_TOPIC", `some comon data: ${new Date().getSeconds()}`)
+}, 3000)
 
 
 server.listen(PORT, () => {
