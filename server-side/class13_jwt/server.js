820b5a04c32cd5555307c87bbbb66d93fc4732b3
M Inzamam Malik <malikasinger1@gmail.com>
2021-01-11 18:20:58 +0500

    today code

---
+++ server-side/class13_jwt/server.js
@@ -0,0 +1,278 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var cors = require("cors");
+var morgan = require("morgan");
+const mongoose = require("mongoose");
+var bcrypt = require("bcrypt-inzi")
+var jwt = require('jsonwebtoken'); // https://github.com/auth0/node-jsonwebtoken
+//is JWT secure? https://stackoverflow.com/questions/27301557/if-you-can-decode-jwt-how-are-they-secure
+
+var SERVER_SECRET = process.env.SECRET || "1234";
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+let dbURI = "mongodb+srv://dbuser:dbpassword@cluster0.9qvbs.mongodb.net/abc-database";
+// let dbURI = 'mongodb://localhost:27017/abc-database';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//https://mongoosejs.com/docs/connections.html#connection-events
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {//connected
+    console.log("Mongoose is connected");
+});
+
+mongoose.connection.on('disconnected', function () {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function (err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+
+
+// https://mongoosejs.com/docs/schematypes.html#what-is-a-schematype
+var userSchema = new mongoose.Schema({
+    "name": String,
+    "email": String,
+    "password": String,
+    "phone": String,
+    "gender": String,
+    "createdOn": { "type": Date, "default": Date.now },
+    "activeSince": Date
+});
+
+// https://mongoosejs.com/docs/models.html
+var userModel = mongoose.model("users", userSchema);
+
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
+
+app.use(morgan('dev'));
+
+
+app.post("/signup", (req, res, next) => {
+
+    if (!req.body.name
+        || !req.body.email
+        || !req.body.password
+        || !req.body.phone
+        || !req.body.gender) {
+
+        res.status(403).send(`
+            please send name, email, passwod, phone and gender in json body.
+            e.g:
+            {
+                "name": "malik",
+                "email": "malikasinger@gmail.com",
+                "password": "abc",
+                "phone": "03001234567",
+                "gender": "Male"
+            }`)
+        return;
+    }
+
+    userModel.findOne({ email: req.body.email },
+        function (err, doc) {
+            if (!err && !doc) {
+
+                bcrypt.stringToHash(req.body.password).then(function (hash) {
+
+                    var newUser = new userModel({
+                        "name": req.body.name,
+                        "email": req.body.email,
+                        "password": hash,
+                        "phone": req.body.phone,
+                        "gender": req.body.gender,
+                    })
+                    newUser.save((err, data) => {
+                        if (!err) {
+                            res.send({
+                                message: "user created"
+                            })
+                        } else {
+                            console.log(err);
+                            res.status(500).send({
+                                message: "user create error, " + err
+                            })
+                        }
+                    });
+                })
+
+            } else if (err) {
+                res.status(500).send({
+                    message: "db error"
+                })
+            } else {
+                res.status(409).send({
+                    message: "user already exist"
+                })
+            }
+        })
+
+})
+
+app.post("/login", (req, res, next) => {
+
+    if (!req.body.email || !req.body.password) {
+
+        res.status(403).send(`
+            please send email and passwod in json body.
+            e.g:
+            {
+                "email": "malikasinger@gmail.com",
+                "password": "abc",
+            }`)
+        return;
+    }
+
+    userModel.findOne({ email: req.body.email },
+        function (err, user) {
+            if (err) {
+                res.status(500).send({
+                    message: "an error occured: " + JSON.stringify(err)
+                });
+            } else if (user) {
+
+                bcrypt.varifyHash(req.body.password, user.password).then(isMatched => {
+                    if (isMatched) {
+                        console.log("matched");
+
+                        var token =
+                            jwt.sign({
+                                id: user._id,
+                                name: user.name,
+                                email: user.email,
+                            }, SERVER_SECRET)
+
+                        res.cookie('jToken', token, {
+                            maxAge: 86_400_000,
+                            httpOnly: true
+                        });
+
+                        // when making request from frontend:
+                        // var xhr = new XMLHttpRequest();
+                        // xhr.open('GET', 'http://example.com/', true);
+                        // xhr.withCredentials = true;
+                        // xhr.send(null);
+
+
+                        res.send({
+                            message: "login success",
+                            user: {
+                                name: user.name,
+                                email: user.email,
+                                phone: user.phone,
+                                gender: user.gender,
+                            }
+                        });
+
+                    } else {
+                        console.log("not matched");
+                        res.status(401).send({
+                            message: "incorrect password"
+                        })
+                    }
+                }).catch(e => {
+                    console.log("error: ", e)
+                })
+
+            } else {
+                res.status(403).send({
+                    message: "user not found"
+                });
+            }
+        });
+
+
+    // read: 
+    // Querying/reading data from database: https://mongoosejs.com/docs/models.html#querying
+    // deleting data from database: https://mongoosejs.com/docs/models.html#deleting
+    // updating data in database: https://mongoosejs.com/docs/models.html#updating
+
+})
+
+app.use(function (req, res, next) {
+
+    console.log("req.cookies: ", req.cookies);
+    if (!req.cookies.jToken) {
+        res.status(401).send("include http-only credentials with every request")
+        return;
+    }
+    jwt.verify(req.cookies.jToken, SERVER_SECRET, function (err, decodedData) {
+        if (!err) {
+
+            const issueDate = decodedData.iat * 1000;
+            const nowDate = new Date().getTime();
+            const diff = nowDate - issueDate; // 86400,000
+
+            if (diff > 300000) { // expire after 5 min (in milis)
+                res.status(401).send("token expired")
+            } else { // issue new token
+                var token = jwt.sign({
+                    id: decodedData.id,
+                    name: decodedData.name,
+                    email: decodedData.email,
+                }, SERVER_SECRET)
+                res.cookie('jToken', token, {
+                    maxAge: 86_400_000,
+                    httpOnly: true
+                });
+                req.body.jToken = decodedData
+                next();
+            }
+        } else {
+            res.status(401).send("invalid token")
+        }
+    });
+})
+
+app.get("/profile", (req, res, next) => {
+
+    console.log(req.body)
+
+    userModel.findById(req.body.jToken.id, 'name email phone gender createdOn',
+        function (err, doc) {
+            if (!err) {
+                res.send({
+                    profile: doc
+                })
+            } else {
+                res.status(500).send({
+                    message: "server error"
+                })
+            }
+        })
+})
+
+app.post("/logout", (req, res, next) => {
+    res.cookie('jToken', "", {
+        maxAge: 86_400_000,
+        httpOnly: true
+    });
+    res.send("logout success");
+})
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("server is running on: ", PORT);
+})
