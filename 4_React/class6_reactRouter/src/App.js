48d85b98d1cf14ed70f449832f0990dc70f91ab5
M Inzamam Malik <malikasinger1@gmail.com>
2021-02-16 15:51:27 +0500

    basic router implemented

--- 4_React/class6_reactRouter/src/App.js
+++ 4_React/class6_reactRouter/src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 import Login from "./components/login/login"
 import Signup from "./components/signup/signup"
 import Dashboard from "./components/dashboard/dashboard"
@@ -8,12 +16,54 @@ import Dashboard from "./components/dashboard/dashboard"
 function App() {
   return (
     <div className="App">
-      
 
-      <Login />
+
+      {/* <Login />
       <Signup />
-      <Dashboard />
+      <Dashboard /> */}
 
+
+
+      <Router>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+
+
+        <Switch>
+
+
+          <Route exact={true} path="/">
+            <Login />
+          </Route>
+
+
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+
+
+          <Route path="/signup">
+            <Signup />
+          </Route>
+
+
+
+
+        </Switch>
+
+      </Router>
 
 
 
